a1400c899bde963f701377fc6bad2f4bdb816795
annesukiassian <anna.sukiassian@outlook.com>
2021-09-24 13:43:40 +0400

    changed argument type

--- src/WordGenerator.java
+++ src/WordGenerator.java
@@ -2,29 +2,35 @@ import java.util.ArrayList;
 
 public class WordGenerator {
 
-    public void generatePossibleWords(int phoneNumber) {
-        String[][] letterArray = new String[][]{
-                {"0"},
-                {"1"},
-                {"A", "B", "C"},
-                {"D", "E", "F"},
-                {"G", "H", "I"},
-                {"J", "K", "L"},
-                {"M", "N", "O"},
-                {"P", "Q", "R", "S"},
-                {"T", "U", "V"},
-                {"W", "X", "Y", "Z"}};
+    public void generatePossibleWords(String inputNumber) {
 
+        if (inputNumber.matches("[0-9]+") && inputNumber.length() > 0) {
 
-        ArrayList<String[]> workingArray = new ArrayList<>();
-        while (phoneNumber > 0) {
-            int digit = phoneNumber % 10;
-            workingArray.add(letterArray[digit]);
-            phoneNumber = phoneNumber / 10;
+            String[][] letterArray = new String[][]{
+                    {"0"},
+                    {"1"},
+                    {"A", "B", "C"},
+                    {"D", "E", "F"},
+                    {"G", "H", "I"},
+                    {"J", "K", "L"},
+                    {"M", "N", "O"},
+                    {"P", "Q", "R", "S"},
+                    {"T", "U", "V"},
+                    {"W", "X", "Y", "Z"}};
+
+            ArrayList<String[]> workingArray = new ArrayList<>();
+
+            for (int i = 0; i < inputNumber.length(); i++) {
+                int digit = Character.getNumericValue(inputNumber.charAt(i));
+                workingArray.add(letterArray[digit]);
+            }
+
+            print(workingArray);
+            System.out.println("Finish");
+        } else {
+            System.out.println("Invalid input");
         }
 
-        print(workingArray);
-        System.out.println("Finish");
 
     }
 
@@ -40,10 +46,15 @@ public class WordGenerator {
         }
 
         while (true) {
+
             // printing current combinations
-            for (int i = n - 1; i >= 0; i--) {
+            for (int i = 0; i < n; i++) {
                 System.out.print(workingArray.get(i)[indexes[i]]);
             }
+
+//            for (int i = n - 1; i >= 0; i--) {
+//                System.out.print(workingArray.get(i)[indexes[i]]);
+//            }
             System.out.println();
 
             // find array which has the most elements after current element
